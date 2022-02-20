e1250b445cb3e3bb4b8d8c089bc416ffaabae653
= <neymegen50@gmail.com>
2022-02-20 13:52:24 +0200

    message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const checkBoxElem = document.querySelector('.task-status');
 checkBoxElem.addEventListener('change', getCheckbox);
 function getCheckbox(event) {
-  if (event.target.checked === true) {
+  if (event.target.checked) {
     console.log(true);
   } else {
     console.log(false);
